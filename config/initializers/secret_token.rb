ec1f6324c304759e46e23d9de5be74085c47c1a8
Eleni Chourouzidou <eleni@webspirit.gr>
2014-08-19 19:47:31 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SplitShipping::Application.config.secret_key_base = '16bddc83edb09902912b62ddedeeb2e2a74108e3d0f1eb582ba7f0f66e641fd8305d8a8e9220803a8347b7a83a432ab572b19e723ef1a9dea5d4d70cede7c57f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SplitShipping::Application.config.session_store :cookie_store, key: '_split_shipping_session'
